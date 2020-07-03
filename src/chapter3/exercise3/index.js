3dbe3feceb9eaa51ab877aed47bac55f69b909f1
Andriy Frankevych <andriy.frankevych@gmail.com>
2020-07-03 17:50:18 +0200

    refactor: remove countBs function

--- src/chapter3/exercise3/index.js
+++ src/chapter3/exercise3/index.js
@@ -1,15 +1,3 @@
-const countBs = string => {
-    let countB = 0;
-
-    for (let i = 0; i <= string.length -1 ; i++) {
-        if (string[i] === 'B') {
-            countB++;
-        }
-    }
-
-    return countB;
-};
-
 const countChar = (string, char) => {
     let countChar = 0;
 
